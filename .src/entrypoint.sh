6bc7e21ce00e2a4dedfdf6133ba531e5acd279a8
Jason Ross <jaross@redhat.com>
2022-07-18 12:45:04 -0500

    add debug to file check

--- .src/entrypoint.sh
+++ .src/entrypoint.sh
@@ -31,7 +31,11 @@ LAST_ARG=""
 
 function check_file() {
     if [ -f $1 ]; then
+        debug "Found File $1"
         add_args $LAST_ARG $1
+    elif [ -f ${GITHUB_WORKSPACE}/$1 ]
+        debug "Found File ${GITHUB_WORKSPACE}/$1"
+        add_args $LAST_ARG "${GITHUB_WORKSPACE}/$1"
     else
         debug "File $1 not found, skipping $LAST_ARG"
         LAST_ARG=""
