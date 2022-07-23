c2fe8d9cbf6920374b1a841a74afaf4214cad812
Jason Ross <jaross@redhat.com>
2022-07-22 19:06:10 -0500

    add package manager

---
+++ write-render-files/script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+## Process input render var into array
+TMPIFS=$IFS
+IFS=$'\n' 
+files=(${RENDER})
+IFS=$TMPIFS
+
+## Change Dir to catalog
+pushd ${DIR}
+
+## Delete existing catalog dir
+find . -name '*.json' -depth 1 -type f -delete
+
+## Iterate over input array
+for data in ${files[@]}
+do
+    ## Set filename from schema
+    file="$(echo $data | jq .schema)"
+    file="$(echo $file | sed s/\"olm\.\\\(\[a\-z\]*\\\)\"/\\1.json/)"
+
+    ## Write File 
+    echo $data >> $file
+done
+
+## Write icon and description Files
+echo $ICON | base64 -d > iconfile
+echo $DESC | base64 -d > descriptionfile
+
+popd
