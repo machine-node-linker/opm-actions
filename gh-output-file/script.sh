3e7030861f833685dcc75d49351de5a7d0043416
Jason Ross <jaross@redhat.com>
2022-07-26 12:07:39 -0500

    fix output-file script

--- gh-output-file/script.sh
+++ gh-output-file/script.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -x
-## Process input render var into array
 
 
 function debug() {
@@ -17,16 +15,16 @@ set -e
 set -E
 
 output="$(cat $FILE)"
-debug("raw file contents: $output")
+debug "raw file contents: $output"
 
 truthy=('true', 'True', 'yes', 'Yes', '1')
 if [[ ${truthy[*]} == *"$B64"* ]]; then
     output=$(echo -n $output | base64)
-    debug("base64 file contents: $output")
+    debug "base64 file contents: $output"
 fi
 
 output="${output//'%'/'%25'}"
 output="${output//$'\n'/'%0A'}"
 output="${output//$'\r'/'%0D'}"
-debug("gh output encoded file contents: $output")
+debug "gh output encoded file contents: $output" 
 echo "::set-output name=output::$output"
